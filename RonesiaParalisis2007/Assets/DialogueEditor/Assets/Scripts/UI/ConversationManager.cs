ba81bd405fe9cc502d065488d70eba9fabdeac6d
Historn <arnaujiga@gmail.com>
2025-04-22 15:36:40 +0200

    Intro dialogue with Certerus finished

--- RonesiaParalisis2007/Assets/DialogueEditor/Assets/Scripts/UI/ConversationManager.cs
+++ RonesiaParalisis2007/Assets/DialogueEditor/Assets/Scripts/UI/ConversationManager.cs
@@ -340,7 +340,7 @@ namespace DialogueEditor
 
         private void ScrollingText_Update()
         {
-            const float charactersPerSecond = 1500;
+            const float charactersPerSecond = 5000;
             float timePerChar = (60.0f / charactersPerSecond);
             timePerChar *= ScrollSpeed;
 
